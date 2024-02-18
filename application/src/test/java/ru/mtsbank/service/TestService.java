165512eddc3017c1480a857440716bc0db5d2296
grpklim <grpklim@gmail.com>
2024-02-18 11:33:38 +0300

    оставил в стартере только фабрику

--- application/src/test/java/ru/mtsbank/service/TestService.java
+++ application/src/test/java/ru/mtsbank/service/TestService.java
@@ -7,15 +7,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import ru.mtsbank.animals.Animal;
 import ru.mtsbank.animals.Cat;
-import ru.mtsbank.config.StarterConfiguration;
 import ru.mtsbank.config.StarterProperties;
 
 import java.math.BigDecimal;
 
-import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
-@SpringBootTest(classes = StarterConfiguration.class)
+@SpringBootTest
 public class TestService {
     @MockBean
     private AnimalFactory factory;
@@ -29,7 +29,7 @@ public class TestService {
     @Test
     public void testCreate() {
         Cat cat = new Cat("Порода", "catName1", "Характер", new BigDecimal("0.0"));
-        when(factory.createAnimal(eq(AnimalType.CAT), anyString(), anyInt())).thenReturn(cat);
+        when(factory.createAnimal(eq(AnimalType.CAT), anyInt())).thenReturn(cat);
         when(properties.getCatNames()).thenReturn(new String[]{"catName1"});
         Animal testCat = service.create(AnimalType.CAT);
         Assertions.assertEquals(cat, testCat);
--- starter/src/main/java/ru/mtsbank/service/AnimalFactory.java
+++ starter/src/main/java/ru/mtsbank/service/AnimalFactory.java
@@ -2,23 +2,40 @@ package ru.mtsbank.service;
 
 import org.springframework.stereotype.Component;
 import ru.mtsbank.animals.*;
+import ru.mtsbank.config.StarterProperties;
 
 import java.math.BigDecimal;
+import java.util.Random;
 
 @Component
 public class AnimalFactory {
-    public Animal createAnimal(AnimalType type, String name, int index) {
+    private StarterProperties starterProperties;
+
+    public AnimalFactory(StarterProperties properties) {
+        starterProperties = properties;
+    }
+
+    public Animal createAnimal(AnimalType type, int index) {
+        String[] names;
         switch (type) {
-            case CAT:
-                return new Cat("Порода " + index, name, "Характер " + index,
+            case CAT -> {
+                names = starterProperties.getCatNames();
+                return new Cat("Порода " + index, names[new Random().nextInt(names.length)], "Характер " + index,
                         new BigDecimal(String.valueOf(index * 5433.232443)));
-            case DOG:
-                return new Dog("Порода " + index, name, "Характер " + index,
+            }
+            case DOG -> {
+                names = starterProperties.getDogNames();
+                return new Dog("Порода " + index, names[new Random().nextInt(names.length)], "Характер " + index,
                         new BigDecimal(String.valueOf(index * 123.3321)));
-            case WOLF:
-                return new Wolf("Порода " + index, name, "Характер " + index);
-            case SHARK:
-                return new Shark("Порода " + index, name, "Характер " + index);
+            }
+            case WOLF -> {
+                names = starterProperties.getWolfNames();
+                return new Wolf("Порода " + index, names[new Random().nextInt(names.length)], "Характер " + index);
+            }
+            case SHARK -> {
+                names = starterProperties.getSharkNames();
+                return new Shark("Порода " + index, names[new Random().nextInt(names.length)], "Характер " + index);
+            }
         }
         return null;
     }
